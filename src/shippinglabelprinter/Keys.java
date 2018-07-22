b7505c2cbb36e69c4d0f82adc1fadb51af8a5160
bobamason <mason94275@gmail.com>
2018-07-22 15:52:09 -0400

    initial commit

---
+++ src/shippinglabelprinter/Keys.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package shippinglabelprinter;
+
+/**
+ *
+ * @author Bob
+ */
+public class Keys {
+
+    public static final String AUTO_PRINT = "autoPrint";
+    public static final String LABEL_PRINTER = "labelPrinter";
+    public static final String NORMAL_PRINTER = "normalPrinter";
+}
